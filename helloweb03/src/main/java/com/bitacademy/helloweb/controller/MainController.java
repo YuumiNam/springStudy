582473648b307b9dc852832939c8bde2f91afc34
dltjsam <dltjsam@gmail.com>
2022-11-15 10:49:39 +0900

    basic

---
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.bitacademy.helloweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *  @RequestMapping 메소드 다중 매핑
+ */
+
+@Controller
+public class MainController {
+	
+	@ResponseBody
+	@RequestMapping({"","/main","/a/b/c/main"})
+	public String main() {
+		return "MainController.main()";
+	}
+}
