f82ce65f4fda2c783bcb7e7e93064571c8d981e2
dltjsam <dltjsam@gmail.com>
2022-11-15 11:33:04 +0900

    basic

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
@@ -2,6 +2,8 @@ package com.bitacademy.helloweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -12,15 +14,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/user")
 public class UserController {
 	
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
+	}
+	
+	// RequrestParam은 없어도 자동으로 만들어지지만 명시 해주는것이 좋다.
 	@ResponseBody
-	@RequestMapping("/joinform")
-	public String joinform() {
-		return "UserController.joinform()";
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/**
+		 *  만일 n이라는 이름의 URL 파라미터가 없는경우
+		 *  400 Bad Request 에러가 발생한다.
+		 */
+		
+		return "UserController.update(" + name + ")";
 	}
 	
 	@ResponseBody
-	@RequestMapping("/join")
-	public String join() {
-		return "UserController.join()";
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value = "n", required = true, defaultValue="") String name) {
+		return "UserController.update2(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value = "p", required = true, defaultValue="1") int pageNo) {
+		return "UserController.list(" + pageNo + ")";
 	}
 }
