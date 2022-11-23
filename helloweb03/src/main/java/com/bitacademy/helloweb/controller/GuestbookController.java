8ef8561571c4ba962dbf58a8d517199312d57e82
dltjsam <dltjsam@gmail.com>
2022-11-23 17:04:20 +0900

    basic

---
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/GuestbookController.java
@@ -0,0 +1,31 @@
+//package com.bitacademy.helloweb.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+///*
+// * 클래스 단독맵핑
+// */
+//@Controller
+//@RequestMapping("/guestbook/*")
+//public class GuestbookController {
+//	
+//	@ResponseBody
+//	@RequestMapping
+//	public String main() {
+//		return "main";
+//	}
+//	
+//	@ResponseBody
+//	@RequestMapping
+//	public String list() {
+//		return "list";
+//	}
+//	
+//	@ResponseBody
+//	@RequestMapping
+//	public String delete() {
+//		return "delete";
+//	}
+//}
